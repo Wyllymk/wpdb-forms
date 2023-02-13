318e8a2be6df84c90e862356c11b2ef7318e81ed
Wilson Mbuthia <wyllyjeremy@gmail.com>
2023-02-13 13:00:38 +0300

    complete

--- wp-content/plugins/contact-us/contact-us.php
+++ wp-content/plugins/contact-us/contact-us.php
@@ -14,8 +14,6 @@
   * Text Domain: contact-us
   */
 
-  //security check
-
 /*-------------------------------------------------------------------------*/
 /*                        SECURITY CHECK                                   */
 /*-------------------------------------------------------------------------*/
@@ -34,27 +32,62 @@ if(!function_exists('add_action')){
 }
 
 /*-------------------------------------------------------------------------*/
-/*                        FETCHING DATA FROM DB                            */
+/*                        CONTACT US CLASS                                 */
 /*-------------------------------------------------------------------------*/
 if(!class_exists('ContactUs')){
-class ContactUs{
+  class ContactUs{
     function __construct(){
-
+        $this->pass_data_to_db();
     }
     function activate(){
+        $this->create_table_to_db();
         flush_rewrite_rules();
     }
     function deactivate(){
         flush_rewrite_rules();
     }
     function create_table_to_db(){
+        global $wpdb;
 
+        $table_name = $wpdb->prefix.'contact';
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $contactus = "CREATE TABLE $table_name(
+            ID bigint unsigned NOT NULL auto_increment,
+            Event_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            Firstname varchar(15) NOT NULL,
+            Lastname varchar(15) NOT NULL,
+            Email varchar(35) NOT NULL,
+            Subject text NOT NULL,
+            PRIMARY KEY (ID)
+        )$charset_collate;";
+        require_once(ABSPATH. 'wp-admin/includes/upgrade.php');
+        dbDelta($contactus);
     }
+
     function pass_data_to_db(){
+        if(isset($_POST['submit'])){
+            $data = array(
+                'Firstname' =>  $_POST['firstname'],
+                'Lastname'  =>  $_POST['lastname'],
+                'Email'     =>  $_POST['email'],
+                'Subject'   =>  $_POST['subject']
 
+            );
+            global $wpdb;
+            $table = 'wp_contact';
+            $result = $wpdb->insert($table, $data, $format=NULL);
+            
+            if($result==true){
+                echo '<script>alert("Form Submitted Successfully");</script>' ;
+            }else{
+                echo '<script>alert("Form Not Submitted");</script>' ;
+            }
+        }
     }
+  }
 }
-}
+//
 $contactUsInstance = new ContactUs();
 
 //activate
